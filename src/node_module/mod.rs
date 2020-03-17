fd87ae6d83c2b25fc8fac305f72683d5c2e6f1fa
nju33 <nju33.ki@gmail.com>
2020-03-18 04:05:37 +0900

    style: rustfmt

--- src/node_module/mod.rs
+++ src/node_module/mod.rs
@@ -8,9 +8,9 @@ use std::path::Path;
 use std::path::PathBuf;
 
 pub enum NodeModuleTypes {
-  Production,
-  Development,
-  Peer,
+    Production,
+    Development,
+    Peer,
 }
 
 ///
@@ -21,53 +21,53 @@ pub enum NodeModuleTypes {
 ///
 #[derive(PartialEq, Eq, PartialOrd, Default, Debug)]
 pub struct NodeModuleType {
-  pub name: String,
-  pub short: char,
+    pub name: String,
+    pub short: char,
 }
 
 impl From<NodeModuleTypes> for NodeModuleType {
-  fn from(types: NodeModuleTypes) -> Self {
-    match types {
-      NodeModuleTypes::Production => NodeModuleType {
-        name: "production".to_owned(),
-        short: 'S',
-      },
-      NodeModuleTypes::Development => NodeModuleType {
-        name: "development".to_owned(),
-        short: 'D',
-      },
-      NodeModuleTypes::Peer => NodeModuleType {
-        name: "peer".to_owned(),
-        short: 'P',
-      },
+    fn from(types: NodeModuleTypes) -> Self {
+        match types {
+            NodeModuleTypes::Production => NodeModuleType {
+                name: "production".to_owned(),
+                short: 'S',
+            },
+            NodeModuleTypes::Development => NodeModuleType {
+                name: "development".to_owned(),
+                short: 'D',
+            },
+            NodeModuleTypes::Peer => NodeModuleType {
+                name: "peer".to_owned(),
+                short: 'P',
+            },
+        }
     }
-  }
 }
 
 impl Ord for NodeModuleType {
-  fn cmp(&self, other: &Self) -> Ordering {
-    if self.short == 'S' {
-      if other.short == 'S' {
-        Ordering::Equal
-      } else {
-        Ordering::Greater
-      }
-    } else if self.short == 'D' {
-      if other.short == 'P' {
-        Ordering::Greater
-      } else if other.short == 'D' {
-        Ordering::Equal
-      } else {
-        Ordering::Less
-      }
-    } else {
-      if other.short == 'P' {
-        Ordering::Equal
-      } else {
-        Ordering::Less
-      }
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self.short == 'S' {
+            if other.short == 'S' {
+                Ordering::Equal
+            } else {
+                Ordering::Greater
+            }
+        } else if self.short == 'D' {
+            if other.short == 'P' {
+                Ordering::Greater
+            } else if other.short == 'D' {
+                Ordering::Equal
+            } else {
+                Ordering::Less
+            }
+        } else {
+            if other.short == 'P' {
+                Ordering::Equal
+            } else {
+                Ordering::Less
+            }
+        }
     }
-  }
 }
 
 // lazy_static! {
@@ -90,56 +90,56 @@ impl Ord for NodeModuleType {
 ///
 #[derive(PartialEq, Eq, PartialOrd, Default, Debug)]
 pub struct NodeModule {
-  pub r#type: NodeModuleType,
-  pub name: String,
-  pub version: String,
-  pub path: PathBuf,
-  pub selected: Cell<bool>,
+    pub r#type: NodeModuleType,
+    pub name: String,
+    pub version: String,
+    pub path: PathBuf,
+    pub selected: Cell<bool>,
 }
 
 impl NodeModule {
-  pub fn new_production(name: String, version: String, path: PathBuf) -> Self {
-    NodeModule {
-      r#type: NodeModuleType::from(NodeModuleTypes::Production),
-      name: name,
-      version: version,
-      path: path,
-      ..Default::default()
+    pub fn new_production(name: String, version: String, path: PathBuf) -> Self {
+        NodeModule {
+            r#type: NodeModuleType::from(NodeModuleTypes::Production),
+            name: name,
+            version: version,
+            path: path,
+            ..Default::default()
+        }
     }
-  }
 
-  pub fn new_development(name: String, version: String, path: PathBuf) -> Self {
-    NodeModule {
-      r#type: NodeModuleType::from(NodeModuleTypes::Development),
-      name: name,
-      version: version,
-      path: path,
-      ..Default::default()
+    pub fn new_development(name: String, version: String, path: PathBuf) -> Self {
+        NodeModule {
+            r#type: NodeModuleType::from(NodeModuleTypes::Development),
+            name: name,
+            version: version,
+            path: path,
+            ..Default::default()
+        }
     }
-  }
 
-  pub fn new_peer(name: String, version: String, path: PathBuf) -> Self {
-    NodeModule {
-      r#type: NodeModuleType::from(NodeModuleTypes::Peer),
-      name: name,
-      version: version,
-      path: path,
-      ..Default::default()
+    pub fn new_peer(name: String, version: String, path: PathBuf) -> Self {
+        NodeModule {
+            r#type: NodeModuleType::from(NodeModuleTypes::Peer),
+            name: name,
+            version: version,
+            path: path,
+            ..Default::default()
+        }
     }
-  }
 }
 
 impl Ord for NodeModule {
-  fn cmp(&self, other: &Self) -> Ordering {
-    let type_ordering = self.r#type.cmp(&other.r#type);
-    let name_ordering = self.name.cmp(&other.name);
+    fn cmp(&self, other: &Self) -> Ordering {
+        let type_ordering = self.r#type.cmp(&other.r#type);
+        let name_ordering = self.name.cmp(&other.name);
 
-    match (type_ordering, name_ordering) {
-      (Ordering::Less, _) | (Ordering::Equal, Ordering::Less) => Ordering::Less,
-      (Ordering::Greater, _) | (Ordering::Equal, Ordering::Greater) => Ordering::Greater,
-      _ => Ordering::Equal,
+        match (type_ordering, name_ordering) {
+            (Ordering::Less, _) | (Ordering::Equal, Ordering::Less) => Ordering::Less,
+            (Ordering::Greater, _) | (Ordering::Equal, Ordering::Greater) => Ordering::Greater,
+            _ => Ordering::Equal,
+        }
     }
-  }
 }
 
 ///
@@ -148,9 +148,9 @@ impl Ord for NodeModule {
 #[allow(non_snake_case)]
 #[derive(Deserialize, Debug)]
 struct Package {
-  dependencies: Option<HashMap<String, String>>,
-  devDependencies: Option<HashMap<String, String>>,
-  peerDependencies: Option<HashMap<String, String>>,
+    dependencies: Option<HashMap<String, String>>,
+    devDependencies: Option<HashMap<String, String>>,
+    peerDependencies: Option<HashMap<String, String>>,
 }
 
 ///
@@ -166,169 +166,169 @@ struct Package {
 ///```
 ///
 pub fn collect_dependencies(current_dir: &Path, filename: &str) -> HashMap<String, NodeModule> {
-  let mut dependency_map: HashMap<String, NodeModule> = HashMap::new();
-  let mut absolute_filepath = current_dir.join(filename);
+    let mut dependency_map: HashMap<String, NodeModule> = HashMap::new();
+    let mut absolute_filepath = current_dir.join(filename);
 
-  let mut dirname = current_dir;
-  while let Ok(json_str) = fs::read_to_string(&absolute_filepath) {
-    let deserialize: Package =
-      serde_json::from_str(&json_str).expect("The package.json seems not json file");
+    let mut dirname = current_dir;
+    while let Ok(json_str) = fs::read_to_string(&absolute_filepath) {
+        let deserialize: Package =
+            serde_json::from_str(&json_str).expect("The package.json seems not json file");
 
-    if let Some(dependencies) = deserialize.dependencies {
-      for key in dependencies.keys() {
-        let key_string = key.to_string();
+        if let Some(dependencies) = deserialize.dependencies {
+            for key in dependencies.keys() {
+                let key_string = key.to_string();
 
-        if let None = dependency_map.get(&key_string) {
-          if let Some(key_value) = dependencies.get(&key_string) {
-            dependency_map.insert(
-              key_string.clone(),
-              NodeModule::new_production(
-                key_string.clone(),
-                key_value.to_owned(),
-                absolute_filepath.to_owned(),
-              ),
-            );
-          }
+                if let None = dependency_map.get(&key_string) {
+                    if let Some(key_value) = dependencies.get(&key_string) {
+                        dependency_map.insert(
+                            key_string.clone(),
+                            NodeModule::new_production(
+                                key_string.clone(),
+                                key_value.to_owned(),
+                                absolute_filepath.to_owned(),
+                            ),
+                        );
+                    }
+                }
+            }
         }
-      }
-    }
 
-    if let Some(dependencies) = deserialize.devDependencies {
-      for key in dependencies.keys() {
-        let key_string = key.to_string();
+        if let Some(dependencies) = deserialize.devDependencies {
+            for key in dependencies.keys() {
+                let key_string = key.to_string();
 
-        if let None = dependency_map.get(&key_string) {
-          if let Some(key_value) = dependencies.get(&key_string) {
-            dependency_map.insert(
-              key_string.clone(),
-              NodeModule::new_development(
-                key_string.clone(),
-                key_value.to_owned(),
-                absolute_filepath.to_owned(),
-              ),
-            );
-          }
+                if let None = dependency_map.get(&key_string) {
+                    if let Some(key_value) = dependencies.get(&key_string) {
+                        dependency_map.insert(
+                            key_string.clone(),
+                            NodeModule::new_development(
+                                key_string.clone(),
+                                key_value.to_owned(),
+                                absolute_filepath.to_owned(),
+                            ),
+                        );
+                    }
+                }
+            }
         }
-      }
-    }
 
-    if let Some(dependencies) = deserialize.peerDependencies {
-      for key in dependencies.keys() {
-        let key_string = key.to_string();
+        if let Some(dependencies) = deserialize.peerDependencies {
+            for key in dependencies.keys() {
+                let key_string = key.to_string();
 
-        if let None = dependency_map.get(&key_string) {
-          if let Some(key_value) = dependencies.get(&key_string) {
-            dependency_map.insert(
-              key_string.clone(),
-              NodeModule::new_peer(
-                key_string.clone(),
-                key_value.to_owned(),
-                absolute_filepath.to_owned(),
-              ),
-            );
-          }
+                if let None = dependency_map.get(&key_string) {
+                    if let Some(key_value) = dependencies.get(&key_string) {
+                        dependency_map.insert(
+                            key_string.clone(),
+                            NodeModule::new_peer(
+                                key_string.clone(),
+                                key_value.to_owned(),
+                                absolute_filepath.to_owned(),
+                            ),
+                        );
+                    }
+                }
+            }
         }
-      }
+
+        if let Some(parent_dir) = &dirname.parent() {
+            dirname = parent_dir;
+        } else {
+            break;
+        }
+
+        absolute_filepath = dirname.join(filename);
     }
 
-    if let Some(parent_dir) = &dirname.parent() {
-      dirname = parent_dir;
-    } else {
-      break;
-    }
-
-    absolute_filepath = dirname.join(filename);
-  }
-
-  dependency_map
+    dependency_map
 }
 
 #[test]
 fn collect_dependencies_returns_correct_map() {
-  use std::env;
+    use std::env;
 
-  let result = collect_dependencies(
-    &env::current_dir()
-      .unwrap()
-      .join("reproduce/parent-node-project/child-node-project"),
-    "package.json",
-  );
+    let result = collect_dependencies(
+        &env::current_dir()
+            .unwrap()
+            .join("reproduce/parent-node-project/child-node-project"),
+        "package.json",
+    );
 
-  assert_eq!(
-    result.get("typescript"),
-    Some(&NodeModule {
-      r#type: NodeModuleType::from(NodeModuleTypes::Production),
-      name: "typescript".to_owned(),
-      version: "^3.8.3".to_owned(),
-      path: env::current_dir()
-        .unwrap()
-        .join("reproduce/parent-node-project/child-node-project/package.json"),
-      selected: Cell::new(false),
-    })
-  );
+    assert_eq!(
+        result.get("typescript"),
+        Some(&NodeModule {
+            r#type: NodeModuleType::from(NodeModuleTypes::Production),
+            name: "typescript".to_owned(),
+            version: "^3.8.3".to_owned(),
+            path: env::current_dir()
+                .unwrap()
+                .join("reproduce/parent-node-project/child-node-project/package.json"),
+            selected: Cell::new(false),
+        })
+    );
 
-  assert_eq!(
-    result.get("redux"),
-    Some(&NodeModule {
-      r#type: NodeModuleType::from(NodeModuleTypes::Production),
-      name: "redux".to_owned(),
-      version: "^4.0.5".to_owned(),
-      path: env::current_dir()
-        .unwrap()
-        .join("reproduce/parent-node-project/package.json"),
-      selected: Cell::new(false),
-    })
-  );
+    assert_eq!(
+        result.get("redux"),
+        Some(&NodeModule {
+            r#type: NodeModuleType::from(NodeModuleTypes::Production),
+            name: "redux".to_owned(),
+            version: "^4.0.5".to_owned(),
+            path: env::current_dir()
+                .unwrap()
+                .join("reproduce/parent-node-project/package.json"),
+            selected: Cell::new(false),
+        })
+    );
 
-  assert_eq!(
-    result.get("eslint"),
-    Some(&NodeModule {
-      r#type: NodeModuleType::from(NodeModuleTypes::Development),
-      name: "eslint".to_owned(),
-      version: "^6.8.0".to_owned(),
-      path: env::current_dir()
-        .unwrap()
-        .join("reproduce/parent-node-project/child-node-project/package.json"),
-      selected: Cell::new(false),
-    })
-  );
+    assert_eq!(
+        result.get("eslint"),
+        Some(&NodeModule {
+            r#type: NodeModuleType::from(NodeModuleTypes::Development),
+            name: "eslint".to_owned(),
+            version: "^6.8.0".to_owned(),
+            path: env::current_dir()
+                .unwrap()
+                .join("reproduce/parent-node-project/child-node-project/package.json"),
+            selected: Cell::new(false),
+        })
+    );
 
-  assert_eq!(
-    result.get("react"),
-    Some(&NodeModule {
-      r#type: NodeModuleType::from(NodeModuleTypes::Peer),
-      name: "react".to_owned(),
-      version: "^16.13.0".to_owned(),
-      path: env::current_dir()
-        .unwrap()
-        .join("reproduce/parent-node-project/package.json"),
-      selected: Cell::new(false),
-    })
-  );
+    assert_eq!(
+        result.get("react"),
+        Some(&NodeModule {
+            r#type: NodeModuleType::from(NodeModuleTypes::Peer),
+            name: "react".to_owned(),
+            version: "^16.13.0".to_owned(),
+            path: env::current_dir()
+                .unwrap()
+                .join("reproduce/parent-node-project/package.json"),
+            selected: Cell::new(false),
+        })
+    );
 
-  assert_eq!(
-    result.get("react-dom"),
-    Some(&NodeModule {
-      r#type: NodeModuleType::from(NodeModuleTypes::Peer),
-      name: "react-dom".to_owned(),
-      version: "^16.13.0".to_owned(),
-      path: env::current_dir()
-        .unwrap()
-        .join("reproduce/parent-node-project/package.json"),
-      selected: Cell::new(false),
-    })
-  );
+    assert_eq!(
+        result.get("react-dom"),
+        Some(&NodeModule {
+            r#type: NodeModuleType::from(NodeModuleTypes::Peer),
+            name: "react-dom".to_owned(),
+            version: "^16.13.0".to_owned(),
+            path: env::current_dir()
+                .unwrap()
+                .join("reproduce/parent-node-project/package.json"),
+            selected: Cell::new(false),
+        })
+    );
 
-  assert_eq!(
-    result.get("next"),
-    Some(&NodeModule {
-      r#type: NodeModuleType::from(NodeModuleTypes::Peer),
-      name: "next".to_owned(),
-      version: "^9.3.0".to_owned(),
-      path: env::current_dir()
-        .unwrap()
-        .join("reproduce/parent-node-project/child-node-project/package.json"),
-      selected: Cell::new(false),
-    })
-  );
+    assert_eq!(
+        result.get("next"),
+        Some(&NodeModule {
+            r#type: NodeModuleType::from(NodeModuleTypes::Peer),
+            name: "next".to_owned(),
+            version: "^9.3.0".to_owned(),
+            path: env::current_dir()
+                .unwrap()
+                .join("reproduce/parent-node-project/child-node-project/package.json"),
+            selected: Cell::new(false),
+        })
+    );
 }
--- src/terminal/mod.rs
+++ src/terminal/mod.rs
@@ -11,108 +11,108 @@ use termion::{color, style};
 /// Function constructs TUI
 ///
 fn build(
-  stdout: &mut AlternateScreen<termion::raw::RawTerminal<std::io::Stdout>>,
-  row: &usize,
-  dependency_list: &[&node_module::NodeModule],
+    stdout: &mut AlternateScreen<termion::raw::RawTerminal<std::io::Stdout>>,
+    row: &usize,
+    dependency_list: &[&node_module::NodeModule],
 ) -> Result<()> {
-  for (i, node_module) in dependency_list.into_iter().enumerate() {
-    let line_number = i + 1_usize;
-    let is_current_line = (*row as u32) == (line_number as u32);
+    for (i, node_module) in dependency_list.into_iter().enumerate() {
+        let line_number = i + 1_usize;
+        let is_current_line = (*row as u32) == (line_number as u32);
 
+        write!(
+            stdout,
+            "{}",
+            termion::cursor::Goto(1, (i as u16) * 2_u16 + 1_u16)
+        )
+        .expect("set the cursor position");
+
+        if node_module.selected.get() {
+            write!(stdout, "{}◉{}", color::Fg(color::Green), style::Reset)
+                .expect("write the checkbox icon");
+        } else {
+            write!(stdout, "◯").expect("write the checkbox icon");
+        }
+
+        write!(stdout, " ").expect("just write a space for layout");
+
+        write!(stdout, "{}", node_module.r#type.short).expect("write the type of the node_module");
+
+        write!(stdout, " ").expect("just write a space for layout");
+
+        if is_current_line {
+            write!(stdout, "{}", termion::style::Underline).expect("emphasize the current line");
+        }
+
+        write!(stdout, "{}@{}", node_module.name, node_module.version)
+            .expect("write a the name and version of a node module");
+
+        if is_current_line {
+            write!(stdout, "{}", termion::style::NoUnderline).expect("reset emphasized");
+        }
+
+        write!(stdout, "\r\n  ").expect("new line and indent");
+
+        write!(
+            stdout,
+            "{}{}{}",
+            color::Fg(color::LightBlack),
+            node_module.path.to_str().unwrap(),
+            termion::style::Reset
+        )
+        .expect("write path in gray");
+    }
+
+    let size = termion::terminal_size().expect("get the terminal size");
     write!(
-      stdout,
-      "{}",
-      termion::cursor::Goto(1, (i as u16) * 2_u16 + 1_u16)
+        stdout,
+        "{}{}",
+        termion::cursor::Goto(size.0, size.1),
+        termion::cursor::Hide
     )
-    .expect("set the cursor position");
-
-    if node_module.selected.get() {
-      write!(stdout, "{}◉{}", color::Fg(color::Green), style::Reset)
-        .expect("write the checkbox icon");
-    } else {
-      write!(stdout, "◯").expect("write the checkbox icon");
-    }
-
-    write!(stdout, " ").expect("just write a space for layout");
-
-    write!(stdout, "{}", node_module.r#type.short).expect("write the type of the node_module");
-
-    write!(stdout, " ").expect("just write a space for layout");
-
-    if is_current_line {
-      write!(stdout, "{}", termion::style::Underline).expect("emphasize the current line");
-    }
-
-    write!(stdout, "{}@{}", node_module.name, node_module.version)
-      .expect("write a the name and version of a node module");
-
-    if is_current_line {
-      write!(stdout, "{}", termion::style::NoUnderline).expect("reset emphasized");
-    }
-
-    write!(stdout, "\r\n  ").expect("new line and indent");
-
-    write!(
-      stdout,
-      "{}{}{}",
-      color::Fg(color::LightBlack),
-      node_module.path.to_str().unwrap(),
-      termion::style::Reset
-    )
-    .expect("write path in gray");
-  }
-
-  let size = termion::terminal_size().expect("get the terminal size");
-  write!(
-    stdout,
-    "{}{}",
-    termion::cursor::Goto(size.0, size.1),
-    termion::cursor::Hide
-  )
-  .expect("hide the cursor");
-  stdout.flush().unwrap();
-  Ok(())
+    .expect("hide the cursor");
+    stdout.flush().unwrap();
+    Ok(())
 }
 
 ///
 /// Function renders TUI
 ///
 pub fn render(dependency_list: &[&node_module::NodeModule]) {
-  let stdin = stdin();
-  let mut stdout = AlternateScreen::from(stdout().into_raw_mode().unwrap());
-  let mut row: usize = 1;
-  write!(stdout, "{}", termion::clear::All).expect("clear terminal");
-  build(&mut stdout, &row, &dependency_list).expect("show dependencies checkboxes");
-  stdout.flush().unwrap();
+    let stdin = stdin();
+    let mut stdout = AlternateScreen::from(stdout().into_raw_mode().unwrap());
+    let mut row: usize = 1;
+    write!(stdout, "{}", termion::clear::All).expect("clear terminal");
+    build(&mut stdout, &row, &dependency_list).expect("show dependencies checkboxes");
+    stdout.flush().unwrap();
 
-  for c in stdin.events() {
-    let event = c.unwrap();
+    for c in stdin.events() {
+        let event = c.unwrap();
 
-    match event {
-      Event::Key(Key::Char('q')) | Event::Key(Key::Ctrl('c')) => break,
-      Event::Key(Key::Char('k')) => {
-        if row > 1 {
-          row -= 1;
+        match event {
+            Event::Key(Key::Char('q')) | Event::Key(Key::Ctrl('c')) => break,
+            Event::Key(Key::Char('k')) => {
+                if row > 1 {
+                    row -= 1;
+                }
+            }
+            Event::Key(Key::Char('j')) => {
+                if row < dependency_list.len() {
+                    row += 1;
+                }
+            }
+            // space
+            Event::Key(Key::Char(' ')) => {
+                dependency_list
+                    .get(row - 1_usize)
+                    .map(|&node_module| node_module.selected.set(!node_module.selected.get()));
+            }
+            // enter
+            Event::Key(Key::Char('\n')) => {
+                break;
+            }
+            Event::Key(_) | Event::Unsupported(_) | Event::Mouse(_) => {}
         }
-      }
-      Event::Key(Key::Char('j')) => {
-        if row < dependency_list.len() {
-          row += 1;
-        }
-      }
-      // space
-      Event::Key(Key::Char(' ')) => {
-        dependency_list
-          .get(row - 1_usize)
-          .map(|&node_module| node_module.selected.set(!node_module.selected.get()));
-      }
-      // enter
-      Event::Key(Key::Char('\n')) => {
-        break;
-      }
-      Event::Key(_) | Event::Unsupported(_) | Event::Mouse(_) => {}
+
+        build(&mut stdout, &row, &dependency_list).expect("rendering");
     }
-
-    build(&mut stdout, &row, &dependency_list).expect("rendering");
-  }
 }
